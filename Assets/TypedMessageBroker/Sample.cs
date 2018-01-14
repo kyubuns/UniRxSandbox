cc6c40d9cd71130e8ca74fa78e68a361181e14a6
kyubuns <kyubuns@gmail.com>
2018-01-14 12:15:59 +0900

    TypedMessageBroker

---
+++ Assets/TypedMessageBroker/Sample.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace TypedMessageBroker.Examples
+{
+    public class Sample : MonoBehaviour
+    {
+        public void Start()
+        {
+            var broker1 = new TypedMessageBroker<IMessageType1>();
+            var broker2 = new TypedMessageBroker<IMessageType2>();
+
+            broker1.Receive<Message1Hoge>().Subscribe(x => Debug.Log($"Message1 Hoge {x.Value}"));
+            broker1.Receive<Message1Fuga>().Subscribe(x => Debug.Log($"Message1 Fuga {x.Value}"));
+
+            broker2.Receive<Message2Hoge>().Subscribe(x => Debug.Log($"Message2 Hoge {x.Value}"));
+            broker2.Receive<Message2Fuga>().Subscribe(x => Debug.Log($"Message2 Fuga {x.Value}"));
+
+            broker1.Publish(new Message1Hoge(0));
+            broker1.Publish(new Message1Fuga(1));
+
+            broker2.Publish(new Message2Hoge(0));
+            broker2.Publish(new Message2Fuga(1));
+
+            // error CS0311: The type `TypedMessageBroker.Examples.Message2Hoge' cannot be used as type parameter `T2' in the generic type or method
+            // broker1.Publish(new Message2Hoge(2));
+        }
+    }
+
+    public interface IMessageType1
+    {
+    }
+
+    public class Message1Hoge : IMessageType1
+    {
+        public Message1Hoge(int value) { Value = value; }
+        public int Value { get; }
+    }
+
+    public class Message1Fuga : IMessageType1
+    {
+        public Message1Fuga(int value) { Value = value; }
+        public int Value { get; }
+    }
+
+    public interface IMessageType2
+    {
+    }
+
+    public class Message2Hoge : IMessageType2
+    {
+        public Message2Hoge(int value) { Value = value; }
+        public int Value { get; }
+    }
+
+    public class Message2Fuga : IMessageType2
+    {
+        public Message2Fuga(int value) { Value = value; }
+        public int Value { get; }
+    }
+}
---
+++ Assets/TypedMessageBroker/TypedMessageBroker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UniRx;
+
+namespace TypedMessageBroker
+{
+    public interface ITypedMessagePublisher<T>
+    {
+        /// <summary>
+        /// Send Message to all receiver.
+        /// </summary>
+        void Publish<T2>(T2 message) where T2 : T;
+    }
+
+    public interface ITypedMessageReceiver<T>
+    {
+        /// <summary>
+        /// Subscribe typed message.
+        /// </summary>
+        IObservable<T2> Receive<T2>() where T2 : T;
+    }
+
+    public interface ITypedMessageBroker<T> : ITypedMessagePublisher<T>, ITypedMessageReceiver<T>
+    {
+    }
+
+    public class TypedMessageBroker<T> : ITypedMessageBroker<T>, IDisposable
+    {
+        private readonly MessageBroker messageBroker = new MessageBroker();
+
+        public void Publish<T2>(T2 message) where T2 : T
+        {
+            messageBroker.Publish(message);
+        }
+
+        public IObservable<T2> Receive<T2>() where T2 : T
+        {
+            return messageBroker.Receive<T2>();
+        }
+
+        public void Dispose()
+        {
+            messageBroker.Dispose();
+        }
+    }
+}
